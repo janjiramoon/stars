8a39531f6804ffc0d5dc2e2be29f7f368dc6b6e8
Janjira Sun <110581158+janjiramoon@users.noreply.github.com>
2024-05-19 22:46:24 -0700

    hiding previous journal entries
    wahooooo

--- base.js
+++ base.js
@@ -45,3 +45,19 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var readMoreButton = document.getElementById('read-more-journal');
+    var targetContent = document.getElementById('journal-read-more');
+
+    readMoreButton.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent default link behavior
+        
+        // Toggle the visibility of the content
+        if (targetContent.style.display === 'block') {
+            targetContent.style.display = 'none'; // Hide the content
+        } else {
+            targetContent.style.display = 'block'; // Show the content
+        }
+    });
+});
